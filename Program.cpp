2fd9e12101d6689f3a877cae4aa8a052710a4af5
sshah93 <stevenssshah75@gmail.com>
2014-04-18 16:35:26 -0400

    Some small changes

--- Program.cpp
+++ Program.cpp
@@ -1,3 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include "libpq-fe.h"
 
 using namespace std;
+
+static void exit_nicely(PGconn *conn)
+{
+	PQfinish(conn);
+	exit(1);
+}
+
+int main()
+{
+	string dbname = "sales";
+	string select_line;
+	string num_of_grouping_vars;
+	string groupingAttr;
+	string fvect;
+	string such_that;
+	string having;
+
+	/* input file for arguments */
+	ifstream fin;	
+	fin.open("input.txt");
+	
+	if(!fin.good())
+	{
+		cout << "Problem with your input file" << endl;
+		return -1;
+	}
+	
+	if(fin.is_open())
+	{
+		while(!fin.eof())
+		{
+			getline(fin, line);
+			
+			// need to do the logic here after shower now :)
+		}
+	}
+}
--- fileParser.cpp
+++ fileParser.cpp
@@ -5,23 +5,5 @@ using namespace std;
 
 int main()
 {
-	string line;
-	ifstream fin;	
-	fin.open("input.txt");
 	
-	if(!fin.good())
-	{
-		cout << "Problem with your input file" << endl;
-		return -1;
-	}
-	
-	if(fin.is_open())
-	{
-		while(!fin.eof())
-		{
-			getline(fin, line);
-			
-			// need to do the logic here after shower now :)
-		}
-	}
 }
